0485f4ff47dda927ca7ad62f86523d7f70c19756
Ариф Ариф <dulev@abv.bg>
2024-10-15 00:41:04 +0300

    модели

---
+++ Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int AvailableCount { get; set; }
+
+        public Book()
+        {
+            Id = 0;
+            Title = "";
+            Isbn = "";
+            Author = "";
+            Genre = "";
+            AvailableCount = 0;
+        }
+    }
+}
---
+++ Models/Reader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Reader
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Egn { get; set; }
+        public string Email { get; set; }
+
+        public Reader()
+        {
+            Id = 0;
+            FirstName = "";
+            LastName = "";
+            Egn = "";
+            Email = "";
+        }
+    }
+}
